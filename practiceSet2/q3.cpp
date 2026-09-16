e74f6e47df742bdfa9029b097c387c9eff35eff7
Avinash Kushwaha <avinashkushwaha7265@gmail.com>
2026-09-16 09:56:36 +0530

    q3

---
+++ practiceSet2/q3.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class Patient {
+    int patientId;
+    string patientName;
+
+    static int activePatients;
+
+public:
+
+    Patient(int id, string name) {
+        patientId = id;
+        patientName = name;
+        activePatients++;
+    }
+
+    ~Patient() {
+        activePatients--;
+    }
+
+
+    static void showActivePatients() {
+        cout << "Active Patients: " << activePatients << endl;
+    }
+};
+
+
+int Patient::activePatients = 0;
+
+int main() {
+
+    Patient p1(101, "Abhay");
+    Patient::showActivePatients();
+
+    {
+        Patient p2(102, "Rahul");
+        Patient p3(103, "Aman");
+
+        Patient::showActivePatients();
+    } 
+
+    Patient::showActivePatients();
+
+    return 0;
+}
